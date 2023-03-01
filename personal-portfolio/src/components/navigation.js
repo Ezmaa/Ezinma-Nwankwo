e703c5bdcac027e4f7a036bc315dba2ca0c22ca0
ezmaa <ezinmark@gmail.com>
2023-02-28 23:13:37 -0500

    config css postioning for navbar, deleted imgs, and fixed navigation.js file name

--- personal-portfolio/src/components/navigation.js
+++ personal-portfolio/src/components/navigation.js
@@ -1,11 +1,10 @@
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
-import githubIcon from '../assets/img/githubIcon.png';
-import linkedinIcon from '../assets/img/linkedinIcon.png';
-import { HashLink } from 'react-router-hash-link';
+import githubIcon from '../assets/img/github-icon.png';
+import linkedinIcon from '../assets/img/linked-in-icon.png';
+
 import {
     BrowserRouter as Router
   } from "react-router-dom";
@@ -37,27 +36,21 @@ export const Navigation = () => {
     <Navbar fixed="top" expand="lg" className={scrolled ? "scrolled": ""}>
       <Container fluid>
         <Navbar.Brand>Ezinma Nwankwo</Navbar.Brand>
-        <div className="social-icon" >
-        <a href="https://github.com/Ezmaa"><img src={githubIcon} alt="" /></a>
-        <a href="https://www.linkedin.com/in/ezinma-nwankwo-3b7905234"><img src={linkedinIcon} alt="" /></a>
-        </div>
 
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => highlightActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => highlightActiveLink('home')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => highlightActiveLink('home')}>Projects</Nav.Link>
           </Nav>
-          <span className="navabr-text">
-            <HashLink to='#connect'>
-            <button className="social-button"><span>Lets Connect</span></button>
-            </HashLink>
+          <span className="navbar-text">
+          <div className="social-icon" >
+        <a href="https://github.com/Ezmaa"><img src={githubIcon} alt="" /></a>
+        <a href="https://www.linkedin.com/in/ezinma-nwankwo-3b7905234"><img src={linkedinIcon} alt="" /></a>
+        </div>
           </span>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
