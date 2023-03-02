a4c92272dc02eae9b2cf316be9c4884b1525a519
ezmaa <ezinmark@gmail.com>
2023-03-02 15:31:38 -0500

    config project card and imported it to project file

---
+++ personal-portfolio/src/components/ProjectCard.js
@@ -0,0 +1,16 @@
+import { Col, Button, Card } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl }) => {
+    return (
+        <Col size={12} sm={6} md={4}>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={imgUrl} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Button variant="primary">Github</Button>
+        </Card.Body>
+      </Card>
+      </Col>
+    )
+}
