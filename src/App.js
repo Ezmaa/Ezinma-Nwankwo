9081cd6a85a298b544d0d0803af1e938654e63e9
ezmaa <ezinmark@gmail.com>
2023-03-23 20:43:17 -0400

    mapped skills instead of carousel for better recruiter experience

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { Navigation } from './components/Navigation';
 import { MainPage } from './components/MainPage';
-import { SkillsCarousel } from './components/SkillsCarousel';
+import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
      <Navigation />
      <MainPage />
-     <SkillsCarousel />
+     <Skills />
      <Projects />
      <Contact />
      <Footer />
---
+++ src/components/Skills.js
@@ -0,0 +1,69 @@
+import { Container, Row, Col } from "react-bootstrap";
+import "react-multi-carousel/lib/styles.css";
+import javascriptIcon from "../assets/img/jsIcon.png";
+import reactIcon from "../assets/img/reactIcon.png";
+import bootstrapIcon from "../assets/img/bootstrapIcon.png";
+import css3Icon from "../assets/img/cssIcon.png";
+import sqlIcon from "../assets/img/mySQLIcon.png";
+import nodeJsIcon from "../assets/img/nodeJsIcon.png";
+import mongoDbIcon from "../assets/img/mongoDbIcon.png";
+import pwaIcon from "../assets/img/pwaIcon.png";
+import restIcon from "../assets/img/restApiIcon.png";
+import { SkillsCard } from "./SkillsCard";
+
+export const Skills = () => {
+  const skills = [
+    {
+      title: "JavaScript",
+      imgUrl: javascriptIcon,
+    },
+    {
+      title: "React",
+      imgUrl: reactIcon,
+    },
+    {
+      title: "Bootstrap",
+      imgUrl: bootstrapIcon,
+    },
+    {
+      title: "CSS3",
+      imgUrl: css3Icon,
+    },
+    {
+      title: "Sql",
+      imgUrl: sqlIcon,
+    },
+    {
+      title: "NodeJs",
+      imgUrl: nodeJsIcon,
+    },
+    {
+      title: "MongoDb",
+      imgUrl: mongoDbIcon,
+    },
+    {
+      title: "PWA",
+      imgUrl: pwaIcon,
+    },
+    {
+      title: "Rest Api",
+      imgUrl: restIcon,
+    },
+  ];
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <h2>Skills</h2>
+            <Row>
+              {skills.map((skill, index) => {
+                return <SkillsCard key={index} {...skill} />;
+              })}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
